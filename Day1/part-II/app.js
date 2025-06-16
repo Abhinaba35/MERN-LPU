d3a38caa54682c9add69f4f36ae0910a10836655
Abhinaba <dasjayanti208@gmail.com>
2025-06-16 14:59:07 +0530

    Day1 - partII

---
+++ Day1/part-II/app.js
@@ -0,0 +1,100 @@
+/*let juice = 50;
+let gstOnjuice = 0.1;
+let fries = 70;
+let gstOnfood = 0.05;
+
+let total = juice + fries + (juice * gstOnjuice) + (fries * gstOnfood);
+
+console.log("Total amout "+ total)
+
+*/
+
+
+/*
+let age = 20;
+console.log("Start", age);
+
+function printBill(name , amount){
+    console.log("---------------");
+    console.log("your bill for", name);
+    console.log("is Rs.", amount);
+    console.log("---------------");
+}
+
+
+//function declaration
+function calculateBillAmount( Price , gst){
+    //parameter
+    let BillAmount = Price + (Price * gst);
+    return BillAmount;
+}
+
+
+//function call
+const res1 = calculateBillAmount(50, 0.3);
+printBill("Juice", res1); // passing argument
+const res2 = calculateBillAmount(50, 0.7);// passing argument
+printBill("Fries", res2); // passing argument
+const res3 =  calculateBillAmount(70, 0.5);
+printBill("Burger", res3); // passing argument
+const res4 = calculateBillAmount(100, 0.5);
+printBill("Pizza", res4); // passing argument
+
+
+function printBill(name , amount){
+    console.log("---------------");
+    console.log("your bill for", name);
+    console.log("is $", amount);
+    console.log("---------------");
+}
+
+console.log("End");
+
+*/
+
+
+/*
+
+function printBillXYZ(name , amount){
+    console.log("---------------");
+    console.log("your bill for", name);
+    console.log("is Rs.", amount);
+    console.log("---------------");
+}
+
+
+//function declaration
+function calculateBillAmountXYZ( Price , gst){
+    //parameter
+    let BillAmount = Price + (Price * gst);
+    return BillAmount;
+}
+
+
+//function call
+const res1 = calculateBillAmount(50, 0.3);
+printBill("Juice", res1); // passing argument
+const res2 = calculateBillAmount(50, 0.7);// passing argument
+printBill("Fries", res2); // passing argument
+const res3 =  calculateBillAmount(70, 0.5);
+printBill("Burger", res3); // passing argument
+const res4 = calculateBillAmount(100, 0.5);
+printBill("Pizza", res4); // passing argument
+
+
+function printBill(name , amount){
+    console.log("---------------");
+    console.log("your bill for", name);
+    console.log("is $", amount);
+    console.log("---------------");
+}
+
+//This gives error
+/*
+function printBillXYZ(name , amount){
+    console.log("---------------");
+    console.log("your bill for", name);
+    console.log("is Rs.", amount);
+    console.log("---------------");
+}
+    */

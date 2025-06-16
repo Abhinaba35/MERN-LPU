2e6da5536ccce75947a93f40dcc49db6ba8af25a
Abhinaba <dasjayanti208@gmail.com>
2025-06-16 13:32:19 +0530

    Day1 - JavaScript

---
+++ Day1/app.js
@@ -0,0 +1,107 @@
+window.console.log("Hello, World!");
+
+//let , const Or var
+/*
+
+let age = 25;
+const year = 2025;
+var block = 100;
+console.log(age , year, block);
+//-----------------------------------
+
+// console.log(block); ---> block is not defined
+console.log(age); //age is undefined
+var age = 25;
+console.log(age);
+var age = 30;
+console.log(age);
+
+//variables have default value of undefined
+
+//------------------------------------
+
+
+
+console.log(age); //age is undefined
+//Cannot access 'age' before initialization
+
+let age = 25;
+console.log(age); 
+
+//let age = 30;
+
+age = 30; //reassigning value to age
+//console.log(age)
+
+
+//-------------------------------------
+
+
+const age = 25;
+console.log(age);
+age = 30; //reassigning value to age not allowed
+
+*/
+
+
+//---------------------------------    !!!!!!!!!!!!!!!!!!       -------------------------------
+
+
+//operators
+
+// Arithmetic Operators
+
+let a = 10;
+
+// Addition
+
+console.log(1+1);
+console.log('1'+1);
+console.log(1+'1');
+console.log('1'+'1');
+
+// Subtraction
+console.log(1-1);
+console.log('a'-1);
+console.log(1-'a');
+console.log('a'-'1');
+
+// Multiplication
+console.log(1*1);
+console.log('a'*1);
+console.log(1*'a');
+console.log('a'*'1');
+
+// Division
+console.log(1/1);
+console.log('a'/2); 
+console.log(1/'a');
+console.log('a'/'1');
+
+
+// Type coercion Implicit
+
+console.log(1 + '1'); // '11'
+console.log('1' + 1); // '11'
+console.log(1 + 1 + '1'); // '21'  
+console.log('1' + 1 + 1); // '111'
+
+console.log('10'- 2);
+console.log('10' - '2'); // 8
+console.log('10' * 2); // 20    
+console.log('10' / 2); // 5
+
+
+console.log(10 -'2');
+console.log(10 * '2'); // 20
+console.log(10 / '2'); // 5
+
+console.log('10' - '2'); // 8
+console.log('10' * '2'); // 20  
+console.log('10' / '2'); // 5
+
+
+
+
+
+

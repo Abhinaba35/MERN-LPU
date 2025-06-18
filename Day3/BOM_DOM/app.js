caab65dbf6450a5eb68c4b64347dae9b05ae3326
Abhinaba <dasjayanti208@gmail.com>
2025-06-18 11:25:53 +0530

    Add initial HTML and JavaScript files for BOM & DOM demonstration

---
+++ Day3/BOM_DOM/app.js
@@ -0,0 +1,19 @@
+//DOM ----> Document Object Model
+//BOM ----> Browser Object Model
+
+
+//console.log("Window ", window);
+//console.log("window.documents" , window.document);
+
+//console.dir(document.children[0].children[1].children[0].innerHTML);
+
+
+//---------------------------------
+
+//1. Document.getElement_s_BytagName --> [] (iterable)(HTML Collections)
+
+const headings = document.getElementsByTagName("h1");
+console.log("Headings " , headings);
+
+
+

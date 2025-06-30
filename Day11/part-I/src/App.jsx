8c57c831836d813060978335e6d3ebee68f18b52
Abhinaba <dasjayanti208@gmail.com>
2025-06-30 10:46:04 +0530

    useEffect + async / await + promises

---
+++ Day11/part-I/src/App.jsx
@@ -0,0 +1,58 @@
+import React, { useEffect, useState } from 'react'
+
+const App = () => {
+/*
+  const getData = () => {
+    const response = fetch("https://dummyjson.com/users");
+
+    response
+      .then((resp) => {
+        const pr2 = resp.json();
+        return pr2;
+      })
+      .catch((err) => {
+        console.log("error:", err.message);
+      })
+      .then((data) => {
+        console.log("data", data);
+      });
+  };
+*/
+
+const [usersList , setUsersList] = useState([]);
+console.log("--START--",usersList);
+
+const getData = async()=> {
+  console.log("Inside getData");
+  const response = await fetch("https://dummyjson.com/users");
+  const data = await response.json();
+  console.log("data --> " , data);
+  const { users } = data;
+  setUsersList(users); 
+}
+
+console.log("before getData call");
+ // getData();
+console.log("getData call");
+
+useEffect(()=>{
+  getData();
+},[]);
+
+//empty depenedency array ==>  it helps function call only once
+
+  return (
+    <div>
+      <h1>Hello</h1>
+      {usersList.map((elem) => {
+        return (
+          <div>
+            <h3>{elem.firstName}</h3>
+          </div>
+        );
+      })}
+    </div>
+  );
+}
+
+export default App
